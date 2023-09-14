41b1322326b6b83afa81cdb2b57d4d1d0ddd9ca8
Arnab Saha <arnabs929@gmail.com>
2023-09-15 02:04:07 +0600

    json data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-
+import CourseList from './components/CourseList'
+import Courses from './components/Courses'
 function App() {
-
   return (
     <div>
- hello world
-  
+    <header><h1 className='text-center text-[#1c1b1b] text-3xl font-bold my-11'>Course Registration</h1></header>
+      <div className='container flex m-auto px-8'>
+        <Courses></Courses>
+        <CourseList></CourseList>
+        </div>
     </div>
   )
 }
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,11 @@
+
+const CourseList = () => {
+    return (
+        <div>
+        <div className="courseListContainer w-1/5 "></div>
+            <h1>Course List</h1>
+        </div>
+    );
+};
+
+export default CourseList;
